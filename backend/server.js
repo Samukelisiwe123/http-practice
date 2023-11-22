ac5d3d7a34f06bcbf5b3536dfc08b23158df383b
Samukelisiwe123 <samkelisiweshongwe69@gmail.com>
2023-11-23 00:02:37 +0200

    added JSON data

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express();
+const fs = require('fs');
+const bodyParser = require("body-parser")
+const cors = require('cors');
+port = 3080;
+
+const usersFilePath = 'users.json';
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const users =loadUsers();
+
+app.get('/api/users', (req,res)=>{
+res.json(users)
+
+});
+app.post('/api/user', (req,resp)=>{
+const user = req.body.user;
+users.push(user); //Insert data into the array
+saveUsers();
+resp.json('user added succesfully');
+});
+app.get('/', (req,res)=>{
+res.json("Server Online")
+})
+app.listen(port, ()=>{
+console.log(`Server listening on port::${port}`)
+});
+
+function loadUsers() {
+
+      const data = fs.readFileSync(usersFilePath, 'utf-8');
+      return JSON.parse(data) || [];
+  }
+
+function saveUsers() {
+      const data = JSON.stringify(users);
+      fs.writeFileSync(usersFilePath, data);
+   
+  }
+  
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
+import { Observable } from 'rxjs';
 @Injectable({
 providedIn: 'root'
 })
 export class ApiService {
 constructor(private http: HttpClient ) { }
 rootURL = 'http://localhost:3080/api';
-url='http://localhost:3080/api/users';
-getUsers() {
-return this.http.get(this.url,{headers:{Accept: 'application/json'}});
-}
-addUser(user: any) {
+
+
+ getUsers(): Observable<any> {
+    return this.http.get(this.rootURL + '/users')
+ }
+
+ addUser(user: any): Observable<any> {
 return this.http.post(this.rootURL + '/user', {user});}
 }
